75e31d291ed142007cadc6928e893b5e8298fb55
ryanczj05 <ryanczj05@gmail.com>
2021-11-14 08:12:09 +0000

    commit 4

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,34 +1,33 @@
 <script>
-    import '../app.scss'; // Bootstrap 5
-    import supabase from '$lib/db';
-    import { page, session } from '$app/stores';
-    import { browser } from '$app/env';
-    import { goto } from '$app/navigation';
+	import '../app.scss';
+	import '../app.scss'; // Bootstrap 5
+	import supabase from '$lib/db';
+	import { page, session } from '$app/stores';
+	import { browser } from '$app/env';
+	import { goto } from '$app/navigation';
 
-    // code that runs only in the browser
-    if (browser) {
-   	 $session = supabase.auth.session(); // set session
-   	 redirect();
+	// code that runs only in the browser
+	if (browser) {
+		$session = supabase.auth.session(); // set session
+		redirect();
 
-   	 supabase.auth.onAuthStateChange((userSession) => {
-   		 $session = userSession; // set session
-   		 redirect();
-   	 });
-    }
+		supabase.auth.onAuthStateChange((userSession) => {
+			$session = userSession; // set session
+			redirect();
+		});
+	}
 
-    function redirect() {
-   	 //login redirect
-   	 if ($session && $page.path === '/') {
-   		 goto('/welcome');
-   	 }
+	function redirect() {
+		//login redirect
+		if ($session && $page.path === '/') {
+			goto('/welcome');
+		}
 
-   	 //logout redirect
-   	 if (!$session && $page.path === '/welcome') {
-   		 goto('/');
-   	 }
-    }
+		//logout redirect
+		if (!$session && $page.path === '/welcome') {
+			goto('/');
+		}
+	}
 </script>
 
 <slot />
-
-
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,5 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <script>
     import supabase from '$lib/db';
 
@@ -22,4 +24,3 @@
    	 <button>Magic</button>
     </form>
 </div>
-
